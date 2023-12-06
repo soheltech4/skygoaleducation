e6e3850b945df29fb07106354bc4d580a2b38ab9
Sohel Rana <sohelranagtc@gmail.com>
2023-12-06 11:10:51 +0600

    add router + navbar

---
+++ src/Shared/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    const nav = <>
+        <>
+            <Link to="/" className={`px-3 hover:font-bold hover:duration-5000 ${location.pathname === '/' ? 'active-link font-bold text-black' : ''}`}>Home</Link>
+            <Link to="/" className={`px-3 hover:font-bold  hover:duration-5000 ${location.pathname === '/about' ? 'active-link font-bold  text-black' : ''}`}>About</Link>
+            <Link to="/" className={`px-3 hover:font-bold  hover:duration-5000 ${location.pathname === '/schedules' ? 'active-link font-bold  text-black' : ''}`}>Schedules</Link>
+            <Link to="/" className={`px-3 hover:font-bold hover:duration-5000 ${location.pathname === '/membership' ? 'active-link font-bold  text-black' : ''}`}>Membership</Link>
+        </>
+    </>
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {nav}
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {nav}
+                </ul>
+            </div>
+            <div className="navbar-end gap-6">
+                <Link className="">Offers</Link>
+                <a className="btn rounded-full">Courses</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/router.jsx
@@ -0,0 +1,23 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+  import "./index.css";
+import Main from "./Main/Main";
+import Home from "./Shared/Home/Home";
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: 
+      [
+        {
+        path: "/",
+        element: <Home></Home>
+        }
+      ]
+
+    },
+  ]);
+
+export default router;
